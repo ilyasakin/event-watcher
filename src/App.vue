53dbd7965e0b3f65af5472eaf0ef595d439a9a40
İlyas Akın <ashnwor@gmail.com>
2021-02-22 22:29:06 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" tabindex="0" ref="main" @keydown="hello">
+    <div>{{ event.key }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, reactive } from 'vue';
+const event = reactive({ key: undefined, code: undefined });
+const hello = (e) => (event.key = e.key);
+
+const main = ref(null);
+onMounted(() => {
+  main.value.focus();
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  height: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ state.count }}</div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const state = reactive({ count: 0 });
+
+const emit = () => console.log('hey');
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
