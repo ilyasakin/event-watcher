327f9ae1a3cef2d0a6e270fff0ae3b51c068f8ba
İlyas Akın <ashnwor@gmail.com>
2021-02-23 19:16:43 +0300

    Use inter, fallback to apple-system then sans-serif

--- src/App.vue
+++ src/App.vue
@@ -32,9 +32,11 @@ const fireEvent = (e) => {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
 #app {
   padding: 2rem;
-  font-family: --apple-system, sans-serif;
+  font-family: Inter, --apple-system, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
